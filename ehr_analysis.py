f7188efdfdeec60bd83c4f1c75568d12da4593d4
“Caitlyn <cn136@duke.edu>
2022-02-23 00:43:24 -0500

    Part 3 upload

--- ehr_analysis.py
+++ ehr_analysis.py
@@ -3,49 +3,204 @@
 Caitlyn Nguyen
 BIOSTAT821
 
-Description:
-This module defines three functions (parse_data, num_older_than, and
-sick_patients) for analyzing EHR data. The EHR data is read from a .txt file
-and is outputted into a list of lists. Each row in the EHR data is an element
-in the outer list with the different EHR variables as elements within the
-inner list. This data structure was used to preserve the order of each
-EHR variable value with the corresponding patient ID and allow for
-multiple EHR entries for the same patient ID.
-
 """
 from datetime import datetime
 
 
-def parse_data(filename: str) -> list[list[str]]:
-    r"""Parse data in text file to a list of lists.
+class Lab:
+    """Lab object."""
+
+    def __init__(
+        self,
+        PATID: str,
+        AdmissionID: str,
+        LabName: str,
+        LabValue: float,
+        LabUnits: str,
+        LabDateTime: datetime,
+    ):
+        """Initialize."""
+        self.PATID = PATID
+        self.AdmissionID = AdmissionID
+        self.LabName = LabName
+        self.LabValue = LabValue
+        self.LabUnits = LabUnits
+        self.LabDateTime = LabDateTime
+
+
+class Patient:
+    """Patient object."""
+
+    def __init__(
+        self,
+        PATID: str,
+        gender: str,
+        DOB: datetime,
+        race: str,
+        marital: str,
+        labs: list[Lab],
+    ):
+        """Initialize."""
+        self.PATID = PATID
+        self.gender = gender
+        self.DOB = DOB
+        self.race = race
+        self.marital = marital
+        self.labs = labs
+        self._lab_dict = None
+
+    @property
+    def age(self):
+        """Calculate patient age."""
+        today = datetime.today()
+        age = (
+            today.year
+            - self.DOB.year
+            - ((today.month, today.day) < (self.DOB.month, self.DOB.day))
+        )
+        return age
+
+    @property
+    def lab_dict(self):
+        """Transform lab objects to dictionary.
+
+        Creates a dictionary with the lab names as keys and
+        a dictionary with the keys "Value", "Units", "Date", and "Age"
+        for the corresponding lab name as the the value.
+
+        Complexity is O(M) as it is required to loop over each lab for
+        the patient, assuming that this is the first call to the property.
+        All other single operations are ignored for big O analysis.
+        """
+        if self._lab_dict is None:  # O(1)
+            lab_dict: dict = dict()  # O(1)
+            for lab in self.labs:  # M times
+                if lab.LabName not in lab_dict.keys():  # O(1)
+                    nested_dict: dict = dict()  # O(1)
+                    nested_dict["Value"] = [lab.LabValue]  # O(1)
+                    nested_dict["Units"] = [lab.LabUnits]  # O(1)
+                    nested_dict["Date"] = [lab.LabDateTime]  # O(1)
+                    age = (
+                        lab.LabDateTime.year
+                        - self.DOB.year
+                        - (
+                            (lab.LabDateTime.month, lab.LabDateTime.day)
+                            < (self.DOB.month, self.DOB.day)
+                        )
+                    )  # O(1)
+                    nested_dict["Age"] = [age]  # O(1)
+                    lab_dict[lab.LabName] = nested_dict  # O(1)
+                else:  # O(1)
+                    nested_dict = lab_dict[lab.LabName]  # O(1)
+                    nested_dict["Value"].append(lab.LabValue)  # O(1)
+                    nested_dict["Units"].append(lab.LabUnits)  # O(1)
+                    nested_dict["Date"].append(lab.LabDateTime)  # O(1)
+                    age = (
+                        lab.LabDateTime.year
+                        - self.DOB.year
+                        - (
+                            (lab.LabDateTime.month, lab.LabDateTime.day)
+                            < (self.DOB.month, self.DOB.day)
+                        )
+                    )  # O(1)
+                    nested_dict["Age"].append(age)  # O(1)
+                    lab_dict[lab.LabName] = nested_dict  # O(1)
+            self._lab_dict = lab_dict  # O(1)
+        return self._lab_dict  # O(1)
+
+    def check_lab_values(self, lab_name: str, gt_lt: str, value: float):
+        """Check if lab values are greater/less than given value.
+
+        Returns True if lab values are greater/less than given value,
+        False if condition is not met.
+
+        Complexity is O(M) assuming self.lab_dict has not been run yet in
+        the worst-case scenario. All other operations are single, which are
+        ignored for big O analysis.
+        """
+        lab_values = self.lab_dict[lab_name]["Value"]  # O(M)
+        if gt_lt == ">":  # O(1)
+            if max(lab_values) > value:  # O(1)
+                return True  # O(1)
+            else:  # O(1)
+                return False  # O(1)
+        elif gt_lt == "<":  # O(1)
+            if min(lab_values) < value:  # O(1)
+                return True  # O(1)
+            else:  # O(1)
+                return False  # O(1)
+        else:  # O(1)
+            raise ValueError("Second input should be '<' or '>'")  # O(1)
+
+
+def parse_data(patient_file: str, lab_file: str) -> list[Patient]:
+    r"""Parse lab and patient data to a list of patient objects.
 
     Assumptions:
-    - filename is a string of the name of the EHR data .txt file.
-    - Each line in the data .txt file is a patient's data.
+    - filename is a string of the name of the lab EHR data .txt file.
+    - Each line in the data .txt file is a lab's data.
     - Each column in the data .txt file is an EHR variable.
     - Values within a row are separated by the tab character '\t\'.
+    - All patients in the patient EHR file have labs in the lab EHR file.
 
     Computational complexity:
-    Opening the file is one operation. It takes another operation
-    to create an empty list. For each line in the file, the line is
-    isolated (first operation), then stripped at all tab characters
-    (second operatoin), and finally appended to a lsit (final operation).
-    These three operations are repeated for N times, making 3*N complexity.
-    The list of lists is then returned for a final single operation.
-    For big-O analysis, the constant factors can be ignored, so
-    this function is of O(N) complexity.
+    The labs are looped over for M*N times to produce a dictionary of labs,
+    where the keys are the PATIDs and the values are a list of Lab objects
+    which correspond to the patient. The patients are looped over for
+    N times to produce a list of Patient objects. For big-O analysis,
+    the constant factors can be ignored, so this function is of
+    O(M*N + N) complexity.
 
     """
-    with open(filename) as file:  # O(1)
+    lab_dictionary: dict = {}  # O(1)
+
+    with open(lab_file) as file:  # O(1)
+        next(file)  # O(1)
+        for line in file:  # M*N times
+            lab_line = line.strip().split("\t")  # O(1)
+            if lab_line[0] not in lab_dictionary.keys():  # O(1)
+                lab_dictionary[lab_line[0]] = [
+                    Lab(
+                        lab_line[0],
+                        lab_line[1],
+                        lab_line[2],
+                        float(lab_line[3]),
+                        lab_line[4],
+                        datetime.strptime(lab_line[5][0:10], "%Y-%m-%d"),
+                    )
+                ]  # O(1)
+            else:  # O(1)
+                lab_dictionary[lab_line[0]].append(
+                    Lab(
+                        lab_line[0],
+                        lab_line[1],
+                        lab_line[2],
+                        float(lab_line[3]),
+                        lab_line[4],
+                        datetime.strptime(lab_line[5][0:10], "%Y-%m-%d"),
+                    )
+                )  # O(1)
+
+    with open(patient_file) as file:  # O(1)
+        next(file)  # O(1)
         results = []  # O(1)
         for line in file:  # N times
-            single_line = line.strip()  # O(1)
-            line_list = single_line.split("\t")  # O(1)
-            results.append(line_list)  # O(1)
+            patient_line = line.strip().split("\t")  # O(1)
+            results.append(
+                Patient(
+                    patient_line[0],
+                    patient_line[1],
+                    datetime.strptime(patient_line[2][0:10], "%Y-%m-%d"),
+                    patient_line[3],
+                    patient_line[4],
+                    lab_dictionary[patient_line[0]],
+                )
+            )  # O(1)
+
     return results  # O(1)
 
 
-def num_older_than(compared_age: float, patient_data: list[list[str]]) -> int:
+def num_older_than(compared_age: float, patient_list: list[Patient]) -> int:
     """Compute the number of individuals older than a given age.
 
     Assumptions:
@@ -56,33 +211,23 @@ def num_older_than(compared_age: float, patient_data: list[list[str]]) -> int:
     - Patient date of birth is recorded as "YYYY-MM-DD".
 
     Computational complexity:
-    Establishing count as 0 and the current day count as two single operations.
-    A for-loop is used to iterate over each row, excluding the header row,
-    for a total of N - 1 times. The patient's birthday is then used to
-    calculate age over 3 operations. It takes one operation to compare age,
-    and one operation to add to count. The for-loop thus has a complexity
-    of 5*(N-1). Returning count is an additional single operation. For
-    big-O analysis, constants can be ignored, leaving a complexity
-    of O(N).
+    Establishing count as 0 is a single operation. A for-loop is used to
+    iterate over each patient for a total of N - 1 times. It takes one
+    operation to compare the patient's age and one operation to add to count.
+    The for-loop thus has a complexity of 2*N. Returning count is an
+    additional single operation. For big-O analysis, constants can be
+    ignored, leaving a complexity of O(N).
 
     """
     count = 0  # O(1)
-    today = datetime.today()  # O(1)
-    for patient in patient_data[1:]:  # N - 1 times
-        DOB = patient[2][0:10]  # O(1)
-        birthday = datetime.strptime(DOB, "%Y-%m-%d")  # O(1)
-        age = (
-            today.year
-            - birthday.year
-            - ((today.month, today.day) < (birthday.month, birthday.day))
-        )  # O(1)
-        if age > compared_age:  # O(1)
+    for patient in patient_list:  # N times
+        if patient.age > compared_age:  # O(1)
             count += 1  # O(1)
     return count  # O(1)
 
 
 def sick_patients(
-    lab: str, gt_lt: str, value: float, lab_data: list[list[str]]
+    lab: str, gt_lt: str, value: float, patient_list: list[Patient]
 ) -> set[str]:
     """Find unique patient IDs with a lab value greater/less than given value.
 
@@ -94,31 +239,22 @@ def sick_patients(
     - Lab value is recorded in the fourth column.
 
     Computational complexity:
-    A blank list is created with a single operation. Each patient encounter is
-    checked to see if the lab name matches the inputted one, for a total
-    of N - 1 operations as the header row is excluded. Up to four operations
-    are then computed for each loop, resulting in 4*(N-1) operations from
-    the for-loop. Return adds another single operation. Constant factors
-    are ignored for big-O analysis, so this is of O(N) complexity.
+    A blank set is created with a single operation. Each patient is checked
+    to see if lab values are greater than or less than given value which uses
+    a function of O(M). It takes a single operation to add the patient ID
+    to the set if the condition is met. Return adds another single operation.
+    Constant factors are ignored for big-O analysis, so this is of
+    O(M*N) complexity.
 
     """
     pat_ids = set()  # O(1)
-    for encounter in lab_data[1:]:  # N - 1 times
-        if encounter[2] == lab:  # O(1)
-            if gt_lt == ">":  # O(1)
-                if float(encounter[3]) > value:  # O(1)
-                    pat_ids.add(encounter[0])  # O(1)
-            elif gt_lt == "<":  # O(1)
-                if float(encounter[3]) < value:  # O(1)
-                    pat_ids.add(encounter[0])  # O(1)
-            else:
-                raise ValueError("Second input should be '<' or '>'")
+    for patient in patient_list:  # N times
+        if patient.check_lab_values(lab, gt_lt, value):  # O(M)
+            pat_ids.add(patient.PATID)  # O(1)
     return pat_ids  # O(1)
 
 
-def age_at_admission(
-    patID: str, patient_data: list[list[str]], lab_data: list[list[str]]
-) -> float:
+def age_at_admission(patID: str, patient_list: list[Patient]) -> float:
     """Compute age at first admission for any given patient.
 
     Assumptions:
@@ -126,55 +262,33 @@ def age_at_admission(
     - All assumptions in the num_older_than function.
     - Lab date is recorded in the sixth column of the lab data file.
     - Lab dates are recorded as "YYYY-MM-DD".
+    - All patients in the patient data have labs in the lab data.
 
     Computational complexity:
     The provided patient ID is searched for within the provided patient data
-    by looping over the patient dataset up to N times, where N = number
-    of patients in the patient dataset. If the patient ID is in the
-    dataset, then the patient's date of birth is recorded and the
-    algorithm stops iterating for a total of 3 operations. If the patient
-    ID is not found in the patient dataset, then a ValueError is raised using
-    two single operations. If the ID is in the dataset, then only
-    two single operations are done. Then a single operation is used
-    to assign patient age to a large number. The algorithm then iterates
-    M times, where M = the number of lab encounters in the lab dataset. Up to
-    six single operations are used to calculate and store the patient's
-    age at a lab encounter if the age is lower than previously checked
-    lab encounters. Finally, if the patient age is not the initially
-    assigned 999 value, then the patient's age at first encounter is
-    returned using two operations. Else, a ValueError is returned to
-    report that the patient ID was not found in the lab data.
-    Constant factors are ignored for big-O analysis, so this
-    function is of O(N + M) complexity, where N = number of patients
-    in the patient dataset, and M = number of lab encounters in the
-    lab dataset.
+    by looping over the patient dataset up to N times. If the patient ID is
+    in the dataset, then the patient's lab data is recorded and the
+    algorithm stops iterating for a total of 3 operations. Recording the lab
+    data is O(M), assuming lab_dict property has not been called upon yet.
+    If the patient ID is not found in the patient dataset, then a
+    ValueError is raised using two single operations. Then a single
+    operation is used to assign patient age to a large number.
+    The algorithm then iterates M times. with three single operations
+    each loop. Constant factors are ignored for big-O analysis, so this
+    function is of O(M*N + M) complexity.
 
     """
-    for patient in patient_data[1:]:  # N times
-        if patID == patient[0]:  # O(1)
-            patient_DOB = patient[2][0:10]  # O(1)
+    for patient in patient_list:  # N times
+        if patient.PATID == patID:  # O(1)
+            lab_data = patient.lab_dict  # O(M)
             break  # O(1)
-        elif patient == patient_data[-1]:  # O(1)
-            raise ValueError(
-                "Patient ID is not found in the provided patient dataset."
-            )  # O(1)
+        elif patient == patient_list[-1]:  # O(1)
+            raise ValueError("Patient is not in the provided dataset.")  # O(1)
 
     patient_age = 999  # O(1)
-    for encounter in lab_data[1:]:  # M times
-        if patID == encounter[0]:  # O(1)
-            encounter_date = datetime.strptime(encounter[5][0:10], "%Y-%m-%d")  # O(1)
-            birthday = datetime.strptime(patient_DOB, "%Y-%m-%d")  # O(1)
-            age = (
-                encounter_date.year
-                - birthday.year
-                - (
-                    (encounter_date.month, encounter_date.day)
-                    < (birthday.month, birthday.day)
-                )
-            )  # O(1)
-            if age < patient_age:  # O(1)
-                patient_age = age  # O(1)
-    if patient_age != 999:  # O(1)
-        return patient_age  # O(1)
-    else:  # O(1)
-        raise ValueError("Patient ID is not found in the provided lab dataset.")  # O(1)
+
+    for lab_values in lab_data.values():  # M times
+        min_age = min(lab_values["Age"])  # O(1)
+        if min_age < patient_age:  # O(1)
+            patient_age = min_age  # O(1)
+    return patient_age  # O(1)
